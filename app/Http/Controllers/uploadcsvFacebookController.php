a928cc51f29cede867b45de448956f6aca00e6fe
fathitarek <ftarek@fcih1.com>
2018-01-15 12:58:17 +0200

    el fun bat3t eslam w ba3mlha call-1

--- app/Http/Controllers/uploadcsvFacebookController.php
+++ app/Http/Controllers/uploadcsvFacebookController.php
@@ -42,24 +42,24 @@ class uploadcsvFacebookController extends Controller {
                 $posts_from_file = csvToArray(public_path() . '/upload/' . $csv_file);
                 for ($i = 0; $i < count($posts_from_file); $i++) {
                     if (!empty($posts_from_file[$i]['message']) && !empty($posts_from_file[$i]['picture']) && !empty($posts_from_file[$i]['created_time']) && $input['date_time'] == '1') {
+                        //create_post(page_type,scheduleDate,scheduleDateTime,picture_url,resource_id,page_id,message,categoryid)
 
-                        
-                        echo '<script type="text/javascript">',
-     'jsfunction();',
-     '</script>'
-;
-                        
+
+                        echo '<script type="text/javascript" script="/js/facebookJavaScript.js">',
+                        'create_post("facebook","",' . $posts_from_file[$i]['created_time'] . ',' . $posts_from_file[$i]["picture"] . ',"0","1494800407485550",' . $posts_from_file[$i]['message'] . ',' . $input['category_id'] . ');',
+                        '</script>';
+
 //                        \DB::table('app_users_posts')->insert(
 //                                [['message' => $posts_from_file[$i]['message'],
 //                                'created_time' => $posts_from_file[$i]['created_time'], 'picture' => $posts_from_file[$i]['picture'], 'app_user_id' => $input['app_user_id']]
 //                        ]);
-                        
-                        
-                        
-                        
-                        
-                        
                     } elseif (!empty($posts_from_file[$i]['message']) && !empty($posts_from_file[$i]['picture']) && $input['date_time'] == '0' && isset($input['category_id']) && !empty($input['category_id'])) {
+
+ echo '<script type="text/javascript" script="/js/facebookJavaScript.js">',
+                        'create_post("facebook","","",' . $posts_from_file[$i]["picture"] . ',"0","1494800407485550",' . $posts_from_file[$i]['message'] . ',' . $input['category_id'] . ');',
+                        '</script>';
+ 
+ 
 //                        \DB::table('app_users_posts')->insert([
 //                            ['message' => $posts_from_file[$i]['message'], 'picture' => $posts_from_file[$i]['picture'],
 //                                'app_user_id' => $input['app_user_id']]
